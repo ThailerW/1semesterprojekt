dd9e0cdb8ececf7864ae234b624f241a264c0807
Theis Werling <thwe53019@edu.ucl.dk>
2023-05-10 10:16:13 +0200

    Added test messagebox

--- SynsPunkt ApS/Form1.cs
+++ SynsPunkt ApS/Form1.cs
@@ -16,5 +16,10 @@ namespace SynsPunkt_ApS
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
