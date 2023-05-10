a2b188c09a1020165c4eca400d37213650b64d5d
Theis Werling <thwe53019@edu.ucl.dk>
2023-05-10 10:24:27 +0200

    Rettet stavefejlen

--- SynsPunkt ApS/Form1.cs
+++ SynsPunkt ApS/Form1.cs
@@ -20,7 +20,7 @@ namespace SynsPunkt_ApS
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Test");
-            MessageBox.Show("testSenbastian");
+            MessageBox.Show("testSebastian");
         }
     }
 }
