fae450f075b08482a48fc2d0fe7ffc021d545a04
Theis Werling <thwe53019@edu.ucl.dk>
2023-05-15 10:46:38 +0200

    Added Object Classes to Models.

---
+++ SynsPunkt ApS/Models/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynsPunkt_ApS.Models
+{
+    public class Booking
+    {
+        public int BookingNummer { get; set; }
+        public DateTime Dato { get; set; }
+        public DateTime Tidspunkt { get; set; }
+        public string BookingType { get; set; }
+
+        public Booking(int bookingNummer, DateTime dato, DateTime tidspunkt, string bookingType)
+        {
+            BookingNummer = bookingNummer;
+            Dato = dato;
+            Tidspunkt = tidspunkt;
+            BookingType = bookingType;
+        }
+    }
+}
