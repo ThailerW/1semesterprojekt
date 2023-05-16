473ff120390c9f0883c9de9b4fdf40be08b8eb7b
Theis Werling <thwe53019@edu.ucl.dk>
2023-05-16 12:12:06 +0200

    Tilføjet metoder til booking

--- SynsPunkt ApS/Database/CRUD_Booking.cs
+++ SynsPunkt ApS/Database/CRUD_Booking.cs
@@ -8,5 +8,21 @@ namespace SynsPunkt_ApS.Database
 {
     public class CRUD_Booking
     {
+        public void CreateBooking()
+        {
+            //logik
+        }
+        public void ReadBooking()
+        {
+            //logik
+        }
+        public void UpdateBooking()
+        {
+            //logik
+        }
+        public void DeleteBooking()
+        {
+            //logik
+        }
     }
 }
