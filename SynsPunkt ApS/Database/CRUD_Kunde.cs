62a3aa0b46d959c8b851853d8918e339a20bffc4
LAPTOP-77Q18SO6\dattu <mtma31730@edu.ucl.dk>
2023-05-22 09:35:21 +0200

    Har tilføjet /// summary for CRUD_Kunde og gjort at koden "matcher" med command.Parameters

--- SynsPunkt ApS/Database/CRUD_Kunde.cs
+++ SynsPunkt ApS/Database/CRUD_Kunde.cs
@@ -27,80 +27,142 @@ namespace SynsPunkt_ApS.Database
         {
             // Logik
         }
-
-
         private string connectionString = "Data Source=mssql15.unoeuro.com;Initial Catalog=oversaftigt_dk_db_test;User ID=oversaftigt_dk;Password=prfHFR9546nEtyb2xDmc";
 
+        /// <summary>
+        /// Opretter en ny kunde i databasen.
+        /// </summary>
+        /// <param name="nyKunde">Den nye kunde, der skal oprettes.</param>
         public void OpretKunde(Kunde nyKunde)
         {
-            string query = $"INSERT INTO Kunder (Fornavn, Efternavn, TelefonNummer, PrivatEmail, Adresse, KundeNummer, KundeInfo) VALUES ('{nyKunde.Fornavn}', '{nyKunde.Efternavn}', '{nyKunde.TelefonNummer}', '{nyKunde.PrivatEmail}', '{nyKunde.Adresse}', '{nyKunde.KundeNummer}', '{nyKunde.KundeInfo}')";
-            ExecuteQuery(query);
-            MessageBox.Show("Kunde oprettet succesfuldt!");
-        }
+            string query = "INSERT INTO Kunder (Fornavn, Efternavn, TelefonNummer, PrivatEmail, Adresse, KundeNummer, KundeInfo) " +
+                           "VALUES (@Fornavn, @Efternavn, @TelefonNummer, @PrivatEmail, @Adresse, @KundeNummer, @KundeInfo)";
 
-        public void HentKundeMedInfo(int KundeInfo)
-        {
-            string query = $"SELECT * FROM Kunder WHERE Id = {KundeInfo}";
-            SqlDataReader reader = ExecuteReader(query);
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    Kunde kunde = new Kunde
-                    (
-                        reader["Fornavn"].ToString(),
-                        reader["Efternavn"].ToString(),
-                        Convert.ToInt32(reader["TelefonNummer"]),
-                        reader["PrivatEmail"].ToString(),
-                        reader["Adresse"].ToString(),
-                        reader["KundeNummer"].ToString(),
-                        reader["KundeInfo"].ToString()
-                    );
-
-                    MessageBox.Show("Kundens oplysninger:\n" +
-                                    "ID: " + kunde.KundeInfo + "\n" +
-                                    "Fornavn: " + kunde.Fornavn + "\n" +
-                                    "Efternavn: " + kunde.Efternavn + "\n" +
-                                    "Email: " + kunde.PrivatEmail + "\n" +
-                                    "Telefonnummer: " + kunde.TelefonNummer + "\n" +
-                                    "Adresse: " + kunde.Adresse);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Kunden blev ikke fundet.");
-            }
-
-            reader.Close();
-        }
-
-        private SqlDataReader ExecuteReader(string query)
-        {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    return command.ExecuteReader();
+                    command.Parameters.AddWithValue("@Fornavn", nyKunde.Fornavn);
+                    command.Parameters.AddWithValue("@Efternavn", nyKunde.Efternavn);
+                    command.Parameters.AddWithValue("@TelefonNummer", nyKunde.TelefonNummer);
+                    command.Parameters.AddWithValue("@PrivatEmail", nyKunde.PrivatEmail);
+                    command.Parameters.AddWithValue("@Adresse", nyKunde.Adresse);
+                    command.Parameters.AddWithValue("@KundeNummer", nyKunde.KundeNummer);
+                    command.Parameters.AddWithValue("@KundeInfo", nyKunde.KundeInfo);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Kunde oprettet succesfuldt!");
+        }
+
+        /// <summary>
+        /// Henter en kunde fra databasen baseret på KundeInfo-id.
+        /// </summary>
+        /// <param name="KundeInfo">Id'et på den ønskede kunde.</param>
+        public void HentKundeMedInfo(int KundeInfo)
+        {
+            string query = "SELECT * FROM Kunder WHERE Id = @KundeInfo";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@KundeInfo", KundeInfo);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Kunde kunde = new Kunde
+                            (
+                                reader["Fornavn"].ToString(),
+                                reader["Efternavn"].ToString(),
+                                Convert.ToInt32(reader["TelefonNummer"]),
+                                reader["PrivatEmail"].ToString(),
+                                reader["Adresse"].ToString(),
+                                reader["KundeNummer"].ToString(),
+                                reader["KundeInfo"].ToString()
+                            );
+
+                            MessageBox.Show("Kundens oplysninger:\n" +
+                                            "ID: " + kunde.KundeInfo + "\n" +
+                                            "Fornavn: " + kunde.Fornavn + "\n" +
+                                            "Efternavn: " + kunde.Efternavn + "\n" +
+                                            "Email: " + kunde.PrivatEmail + "\n" +
+                                            "Telefonnummer: " + kunde.TelefonNummer + "\n" +
+                                            "Adresse: " + kunde.Adresse);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Kunden blev ikke fundet.");
+                    }
+
+                    reader.Close();
                 }
             }
         }
 
+        /// <summary>
+        /// Opdaterer en kunde i databasen.
+        /// </summary>
+        /// <param name="opdateretKunde">Den opdaterede kundeoplysninger.</param>
         public void OpdaterKunde(Kunde opdateretKunde)
         {
-            string query = $"UPDATE Kunder SET Fornavn = '{opdateretKunde.Fornavn}', Efternavn = '{opdateretKunde.Efternavn}', Email = '{opdateretKunde.PrivatEmail}', Telefonnummer = '{opdateretKunde.KundeNummer}', Adresse = '{opdateretKunde.Adresse}' WHERE Id = {opdateretKunde.KundeInfo.ToString()}";
-            ExecuteQuery(query);
+            string query = "UPDATE Kunder SET Fornavn = @Fornavn, Efternavn = @Efternavn, PrivatEmail = @PrivatEmail, " +
+                           "TelefonNummer = @TelefonNummer, Adresse = @Adresse WHERE Id = @KundeInfo";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Fornavn", opdateretKunde.Fornavn);
+                    command.Parameters.AddWithValue("@Efternavn", opdateretKunde.Efternavn);
+                    command.Parameters.AddWithValue("@PrivatEmail", opdateretKunde.PrivatEmail);
+                    command.Parameters.AddWithValue("@TelefonNummer", opdateretKunde.KundeNummer);
+                    command.Parameters.AddWithValue("@Adresse", opdateretKunde.Adresse);
+                    command.Parameters.AddWithValue("@KundeInfo", opdateretKunde.KundeInfo.ToString());
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
             MessageBox.Show("Kunde opdateret succesfuldt!");
         }
 
+        /// <summary>
+        /// Sletter en kunde fra databasen baseret på Id.
+        /// </summary>
+        /// <param name="id">Id'et på kunden, der skal slettes.</param>
         public void SletKunde(int id)
         {
-            string query = $"DELETE FROM Kunder WHERE Id = {id}";
-            ExecuteQuery(query);
+            string query = "DELETE FROM Kunder WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
             MessageBox.Show("Kunde slettet succesfuldt!");
         }
 
+        /// <summary>
+        /// Udfører en generel databaseforespørgsel.
+        /// </summary>
+        /// <param name="query">SQL-forespørgslen, der skal udføres.</param>
         private void ExecuteQuery(string query)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -111,9 +173,8 @@ namespace SynsPunkt_ApS.Database
                     command.ExecuteNonQuery();
                 }
             }
-
-
         }
     }
-
 }
+
+
