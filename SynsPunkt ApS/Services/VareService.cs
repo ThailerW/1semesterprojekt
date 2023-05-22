f6bc3be9ff169bd269b41d49ffa084f178663eda
Theis Werling <thwe53019@edu.ucl.dk>
2023-05-22 09:30:57 +0200

    VareService

---
+++ SynsPunkt ApS/Services/VareService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynsPunkt_ApS.Services
+{
+    public class VareService
+    {
+
+    }
+}
